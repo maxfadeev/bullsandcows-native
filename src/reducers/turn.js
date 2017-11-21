48d8523fa24cc56cb0b904cdff0ad56e531398a1
maxfadeev <oxinhood@gmail.com>
2017-11-21 16:35:10 +0200

    Extend the turn reducer

--- src/reducers/turn.js
+++ src/reducers/turn.js
@@ -1,5 +1,15 @@
-const turn = (state = true, action) => {
-  return state
+import { GUESS_TURN, SCORE_TURN } from '../constants/Game'
+import { ADD_GUESS, ADD_SCORE } from '../constants/ActionTypes'
+
+const turn = (state = GUESS_TURN, action) => {
+  switch (action.type) {
+    case ADD_GUESS:
+      return SCORE_TURN
+    case ADD_SCORE:
+      return GUESS_TURN
+    default:
+      return state
+  }
 }
 
 export default turn
