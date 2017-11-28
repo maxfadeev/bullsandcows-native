0518220fc73624ee65b2c32026ac094ac9d24d29
maxfadeev <masdeft@gmail.com>
2017-11-28 20:18:50 +0200

    Add LayoutAnimation to NumericButtonsList

--- src/components/NumericButtonsList.js
+++ src/components/NumericButtonsList.js
@@ -1,39 +1,70 @@
 import React from 'react'
-import { FlatList, StyleSheet } from 'react-native'
+import {
+  FlatList,
+  StyleSheet,
+  UIManager,
+  LayoutAnimation,
+  View,
+  Platform,
+  NativeModules
+} from 'react-native'
 
 import NumericButton from './NumericButton'
 
 export default class NumericButtonsList extends React.Component {
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.numericButtonsVisibility === false) {
+  constructor() {
+    super()
+    this.state = {
+      top: minTopPosition
+    }
+
+    if (Platform.OS === 'android') {
+      NativeModules.UIManager.setLayoutAnimationEnabledExperimental &&
+        NativeModules.UIManager.setLayoutAnimationEnabledExperimental(true)
     }
   }
 
-  shouldComponentUpdate() {
-    return false
+  componentWillReceiveProps(nextProps) {
+    LayoutAnimation.spring()
+    if (nextProps.numericButtonsVisibility === false) {
+      this.setState({ top: 200 })
+    } else {
+      this.setState({ top: minTopPosition })
+    }
   }
 
   render() {
     const { numerals, turn, onNumericButtonPress } = this.props
     return (
-      <FlatList
-        style={styles.flatList}
-        numColumns={5}
-        data={numerals.map((n, i) => ({ key: i, numeral: n }))}
-        renderItem={({ item }) => (
-          <NumericButton
-            onPress={() => onNumericButtonPress(item.numeral, turn)}
-          >
-            {item.numeral}
-          </NumericButton>
-        )}
-      />
+      <View style={[styles.view, { top: this.state.top }]}>
+        <FlatList
+          style={styles.flatList}
+          numColumns={5}
+          data={numerals.map((n, i) => ({ key: i, numeral: n }))}
+          renderItem={({ item }) => (
+            <NumericButton
+              onPress={() => onNumericButtonPress(item.numeral, turn)}
+            >
+              {item.numeral}
+            </NumericButton>
+          )}
+        />
+      </View>
     )
   }
 }
+const minTopPosition = 15
 
 const styles = StyleSheet.create({
   flatList: {
     width: 250
+  },
+  view: {
+    position: 'absolute',
+    top: minTopPosition,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    alignItems: 'center'
   }
 })
