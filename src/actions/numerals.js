a00bfcc9814821409a93bab0a311d612dbb4016b
maxfadeev <oxinhood@gmail.com>
2017-11-29 16:09:30 +0200

    Add PRESS_ROUND_BUTTON action

--- src/actions/numerals.js
+++ src/actions/numerals.js
@@ -2,7 +2,8 @@ import {
   PRESS_NUMERIC_BUTTON,
   REMOVE_TYPED_DIGIT,
   TOGGLE_NUMERIC_BUTTONS_VISIBILITY,
-  SHOW_NUMERIC_BUTTONS
+  SHOW_NUMERIC_BUTTONS,
+  PRESS_ROUND_BUTTON
 } from '../constants/ActionTypes'
 
 export const pressNumericButton = (numeral, turn) => {
@@ -24,5 +25,11 @@ export const discardDigit = (numeral, key) => {
 export const toggleNumericButtonsVisibility = () => {
   return {
     type: TOGGLE_NUMERIC_BUTTONS_VISIBILITY
+  }
+}
+
+export const pressRoundButton = () => {
+  return {
+    type: PRESS_ROUND_BUTTON
   }
 }
