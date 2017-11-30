c97e1c11d909cb8be2d65106f4e08fa732069dde
maxfadeev <oxinhood@gmail.com>
2017-11-30 15:06:27 +0200

    Test numerals actions

---
+++ src/actions/numerals.spec.js
@@ -0,0 +1,46 @@
+import * as actions from './numerals'
+import * as types from '../constants/ActionTypes'
+import * as gameConstants from '../constants/Game'
+
+describe('numerals actions', () => {
+  it('should create an action to press a numeric button', () => {
+    const numeral = 3
+    const turn = gameConstants.GUESS_TURN
+    const expectedAction = {
+      type: types.PRESS_NUMERIC_BUTTON,
+      numeral,
+      turn
+    }
+    expect(actions.pressNumericButton(numeral, turn)).toEqual(expectedAction)
+  })
+
+  it('should create an action to discard a digit', () => {
+    const numeral = 3
+    const key = 4
+    const expectedAction = {
+      type: types.DISCARD_TYPED_DIGIT,
+      numeral,
+      key
+    }
+    expect(actions.discardDigit(numeral, key)).toEqual(expectedAction)
+  })
+
+  it('should create an action to toggle numeric buttons visibility', () => {
+    const expectedAction = {
+      type: types.TOGGLE_NUMERIC_BUTTONS_VISIBILITY
+    }
+    expect(actions.toggleNumericButtonsVisibility()).toEqual(expectedAction)
+  })
+
+  it('should create an action to press a round button', () => {
+    const turn = gameConstants.GUESS_TURN
+    const typedDigits = [1, 2, 3, 4]
+    const expectedAction = {
+      type: types.PRESS_ROUND_BUTTON,
+      turn,
+      yourDigits: typedDigits,
+      opponentsDigits: [1, 2, 3, 4]
+    }
+    expect(actions.pressRoundButton(turn, typedDigits)).toEqual(expectedAction)
+  })
+})
