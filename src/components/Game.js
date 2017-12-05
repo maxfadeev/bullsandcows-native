b5c6fd34a38a62266acc94c2520d747e106ee247
maxfadeev <masdeft@gmail.com>
2017-12-05 08:41:47 +0200

    Fix buttons muving behind window

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,7 +12,7 @@ const Game = () => (
     <View style={{ flex: 1 }}>
       <TypedDigitsContainer />
     </View>
-    <View style={{ flex: 3 }}>
+    <View style={{ flex: 3, width: 300 }}>
       <NumericButtonsContainer numerals={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]} />
       <RoundButtonContainer />
     </View>
