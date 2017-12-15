5de5f2c31dbd44f488344cdf90f7f5f1ece67d33
maxfadeev <oxinhood@gmail.com>
2017-12-15 15:40:55 +0200

    Add more tests to typedDigits spec

--- src/reducers/typedDigits.spec.js
+++ src/reducers/typedDigits.spec.js
@@ -1,54 +1,113 @@
 import reducer from './typedDigits'
 import * as types from '../constants/ActionTypes'
-import * as gameConstants from '../constants/Game'
+import {
+  SUB,
+  GUESS_TURN,
+  SCORE_TURN,
+  SECRET_LENGTH,
+  SCORE_LENGTH
+} from '../constants/Game'
 
 describe('typedDigits reducer', () => {
-  const initialState = Array(gameConstants.SECRET_LENGTH).fill(
-    gameConstants.SUB
-  )
+  const initialState = Array(SECRET_LENGTH).fill(SUB)
 
-  describe('handle PRESS_NUMERIC_BUTTON', () => {
-    it('should return the initial state', () => {
-      expect(reducer(undefined, {})).toEqual(initialState)
-    })
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  describe('handle PRESS_NUMERIC_BUTTON on the guess turn', () => {
+    const turn = GUESS_TURN
 
     it('should add the numeral', () => {
       const numeral = 7
-      const turn = gameConstants.GUESS_TURN
-      const sub = gameConstants.SUB
       expect(
         reducer(initialState, {
           type: types.PRESS_NUMERIC_BUTTON,
           numeral,
           turn
         })
-      ).toEqual([numeral, sub, sub, sub])
+      ).toEqual([numeral, SUB, SUB, SUB])
     })
 
-    it('should not add the numeral if it is a guess turn, it is the first one numeral and equals 0', () => {
+    it('should not add the numeral if it is the first one numeral and equals 0', () => {
       const numeral = 0
-      const turn = gameConstants.GUESS_TURN
-      const sub = gameConstants.SUB
       expect(
         reducer(initialState, {
           type: types.PRESS_NUMERIC_BUTTON,
           numeral,
           turn
         })
-      ).toEqual([sub, sub, sub, sub])
+      ).toEqual([SUB, SUB, SUB, SUB])
     })
 
-    it('should not add the number if it is a guess turn and the numeral already exists', () => {
+    it('should not add the numeral if it already exists', () => {
       const numeral = 7
-      const turn = gameConstants.GUESS_TURN
-      const sub = gameConstants.SUB
       expect(
-        reducer(initialState, {
+        reducer([numeral, SUB, SUB, SUB], {
           type: types.PRESS_NUMERIC_BUTTON,
           numeral,
           turn
         })
-      ).toEqual([numeral, sub, sub, sub])
+      ).toEqual([numeral, SUB, SUB, SUB])
+    })
+
+    it(`should not add the numeral if there are already ${SECRET_LENGTH} typed numerals`, () => {
+      const numeral = 7
+      expect(
+        reducer([1, 2, 3, 4], {
+          type: types.PRESS_NUMERIC_BUTTON,
+          numeral,
+          turn
+        })
+      ).toEqual([1, 2, 3, 4])
+    })
+  })
+
+  describe('handle PRESS_NUMERIC_BUTTON on the score turn', () => {
+    const turn = SCORE_TURN
+
+    it(`should add the numeral if it is less than or equal ${SECRET_LENGTH}`, () => {
+      const numeral = 3
+      expect(
+        reducer([SUB, SUB], {
+          type: types.PRESS_NUMERIC_BUTTON,
+          numeral,
+          turn
+        })
+      ).toEqual([numeral, SUB])
+    })
+
+    it(`should not add the numeral if it is greater than ${SECRET_LENGTH}`, () => {
+      const numeral = 8
+      expect(
+        reducer([SUB, SUB], {
+          type: types.PRESS_NUMERIC_BUTTON,
+          numeral,
+          turn
+        })
+      ).toEqual([SUB, SUB])
+    })
+
+    it(`should not add the numeral if a sum is greater than ${SECRET_LENGTH}`, () => {
+      const numeral = 3
+      expect(
+        reducer([2, SUB], {
+          type: types.PRESS_NUMERIC_BUTTON,
+          numeral,
+          turn
+        })
+      ).toEqual([2, SUB])
+    })
+
+    it(`should not add the numeral if there are already ${SCORE_LENGTH} typed numerals`, () => {
+      const numeral = 1
+      expect(
+        reducer([1, 2], {
+          type: types.PRESS_NUMERIC_BUTTON,
+          numeral,
+          turn
+        })
+      ).toEqual([1, 2])
     })
   })
 })
